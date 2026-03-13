f3c37c2c221860b1f5052e834d907365025b24f9
Sauqi <iquuwi@gmail.com>
2026-03-13 17:57:01 +0700

    Add files via upload

---
+++ Hasil.py
@@ -0,0 +1,10 @@
+1- Hasil Perconaa setelah di run 5 kali:
+2- Hasil Percobaan
+     Percobaan 1 = 100000
+     Percobaan 2 = 100002
+     Percobaan 3 = 100009
+     Percobaan 4 = 100012
+     Percobaan 5 = 100003
+3- Penyebab inkonsistensi : adanya interleave antar thread tanpa adanya sinkronisasi
+4- Interleaving merujuk pada eksekusi intruksi dari beberapa thread yang saling tumpang
+tindih secara bergantian, hal tersebut yang menyebabkan hilangnya data pembaruan.
